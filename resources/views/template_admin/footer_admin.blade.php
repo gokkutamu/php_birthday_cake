555c6ec7632f3b8dc6a1ab721db8f383e4e80aa1
Mr TAM <ngoctam2303001@gmail.com>
2021-09-17 11:27:34 +0700

    birthday cake sale page

---
+++ resources/views/template_admin/footer_admin.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+         
+            <div class="copy text-center">
+               Copyright 2017 <a href='#'>Website Ngọc Tâm development</a></br>
+            </div>
+            
+         </div>
